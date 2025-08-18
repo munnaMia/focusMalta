56a00926c1e286b50d08c4fef1302e6fc07020dd
Md Munna Mia <mdmunnamia987@gmail.com>
2025-08-18 10:51:11 +0600

    add: info & error logger to application dependencies

--- cmd/focusMalta/main.go
+++ cmd/focusMalta/main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
-type application struct{}
+type application struct {
+	InfoLog  *slog.Logger
+	ErrorLog *slog.Logger
+}
 
 func main() {
-	app := application{}
+	// logger handlers
+	infoHanlder := slog.NewJSONHandler(
+		os.Stdout,
+		&slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		})
+
+	errorHanlder := slog.NewJSONHandler(
+		os.Stdout,
+		&slog.HandlerOptions{
+			Level:     slog.LevelError,
+			AddSource: true,
+		})
+
+	// loggers.
+	infoLog := slog.New(infoHanlder)
+	errorLog := slog.New(errorHanlder)
+
+	// application dependencis
+	app := &application{
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+	}
 
 	// contain  server configurations.
 	server := http.Server{
@@ -16,8 +42,15 @@ func main() {
 		Handler: app.routes(),
 	}
 
+	// Server Runing massage.
+	infoLog.Info(
+		"Server Running",
+		slog.String("PORT", server.Addr),
+	)
+
 	err := server.ListenAndServe()
 	if err != nil {
-		log.Fatalln(err)
+		errorLog.Error(err.Error())
+		os.Exit(1)
 	}
 }
