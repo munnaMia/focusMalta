0f71791b5ce44e6b60c21695d85093d9d56d88a9
Md Munna Mia <mdmunnamia987@gmail.com>
2025-08-19 15:52:38 +0600

    add: about route

--- cmd/focusMalta/routes.go
+++ cmd/focusMalta/routes.go
@@ -5,7 +5,8 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", app.home)
+	mux.Handle("GET /", http.HandlerFunc(app.home))
+	mux.Handle("GET /about", http.HandlerFunc(app.about))
 
 	return mux
 }
