499e0b608ed681146fac2ac5206854c0f5346f63
Md Munna Mia <mdmunnamia987@gmail.com>
2025-08-19 15:53:38 +0600

    add: about handler & update: home handler

--- cmd/focusMalta/handlers.go
+++ cmd/focusMalta/handlers.go
@@ -1,7 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to focus malta"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./cmd/ui/html/base.html",
+		"./cmd/ui/html/pages/home.html",
+		"./cmd/ui/html/partials/nav.html",
+		"./cmd/ui/html/partials/footer.html",
+		"./cmd/ui/html/partials/header.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.ErrorLog.Error(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.ErrorLog.Error(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (app *application) about(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("about us"))
 }
