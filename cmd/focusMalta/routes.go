fd9cfc024774136dc90fadb55ac13478f7f19dab
Md Munna Mia <mdmunnamia987@gmail.com>
2025-08-19 17:21:36 +0600

    add: file server

--- cmd/focusMalta/routes.go
+++ cmd/focusMalta/routes.go
@@ -5,6 +5,10 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	// file server to serve static files
+	fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir("./cmd/ui/static")))
+	mux.Handle("GET /static/", fileServer)
+
 	mux.Handle("GET /", http.HandlerFunc(app.home))
 	mux.Handle("GET /about", http.HandlerFunc(app.about))
 
